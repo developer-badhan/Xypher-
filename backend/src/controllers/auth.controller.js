f1033f53afd2ecb7cd9aea31e59c598e5c508531
Badhan Sarkar <badhansarkar164@gmail.com>
2025-10-11 20:47:26 +0530

    feat: enhance login functionality with password verification and update logout route to use POST method

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -72,11 +72,41 @@ export const signup = async (req,res) => {
 
 }
 
-export const login = (req,res) => {
-    res.send("Login endpoint");
+export const login = async (req,res) => {
+    const {email ,password} = req.body;
+
+    try{
+        const user = await User.findOne({email});
+        if(!user) return res.status(400).json({message:"Invalid credentials"});
+
+        const isPasswordCorrect = await bcrypt.compare(password,user.password);
+        if(!isPasswordCorrect){
+            return res
+            .status(400)
+            .json({
+                message : "Invalid credentials"
+            });
+        }
+
+        generateToken(user._id,res);
+        res.status(200).json({
+            _id:user._id,
+            fullName:user.fullName,
+            email:user.email,
+            profilePic : user.profilePic
+        });
+
+    } catch(error) {
+        console.error("Login error:",error);
+        res.status(500).json({message:"Internal server error"});
+    }
 }
 
-export const logout = (req,res) => {
-    res.send("Logout endpoint");
+export const logout = async (req,res) => {
+    res.cookie("jwt","",{
+        maxAge: 0,
+        httpOnly: true
+    });
+    res.status(200).json({message:"Logout successful"});
 }
 

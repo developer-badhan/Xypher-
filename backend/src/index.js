893db6633fb341961d10e0ad5523e32d894a610d
Badhan Sarkar <badhansarkar164@gmail.com>
2025-10-11 21:24:30 +0530

    feat: add cookie parser middleware for improved request handling

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.route.js";
 import messageRouter from "./routes/message.route.js";
 import connectDB from "./config/db.js";
@@ -10,6 +10,7 @@ const app = express();
 const port = ENV.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/v1/auth",authRouter);
 app.use("/api/v1/message",messageRouter);
